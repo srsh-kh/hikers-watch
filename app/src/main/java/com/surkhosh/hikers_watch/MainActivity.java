4b0ed4f7ecdb6bc2906f52c88db523fe6123256d
srsh-kh <sur.khosh@gmail.com>
2023-01-16 18:00:03 +0330

    x

--- app/src/main/java/com/surkhosh/hikers_watch/MainActivity.java
+++ app/src/main/java/com/surkhosh/hikers_watch/MainActivity.java
@@ -1,14 +1,35 @@
 package com.surkhosh.hikers_watch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.i("Location", location.toString());
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) );
     }
 }
